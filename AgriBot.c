13219841f49f46fc8fce0a0383f48411856a5409
Abdulrhman Atassi <65854575+abdulrhmanAtassi@users.noreply.github.com>
2025-05-11 16:59:17 +0300

    Update AgriBot.c

--- AgriBot.c
+++ AgriBot.c
@@ -5,6 +5,24 @@ void our_delay_ms(unsigned int ms) {
     }
 }
 
+void setupPWM() {
+    // Set RC2 (CCP1) and RC1 (CCP2) as output
+    TRISC2 = 0;
+    TRISC1 = 0;
+
+    // Set PWM mode for CCP1 and CCP2
+    CCP1CON = 0b00001100;  // PWM mode
+    CCP2CON = 0b00001100;  // PWM mode
+
+    // Set PWM frequency (~5kHz for Fosc = 20MHz)
+    PR2 = 249;
+    T2CON = 0b00000101; // Prescaler = 4, Timer2 ON
+
+    // Set initial duty cycles (e.g., 50%)
+    CCPR1L = 128; // Left motors
+    CCPR2L = 128; // Right motors
+}
+
 void setup(){
      TRISD = 0x00; // Set PORTD as output
      PORTD = 0x00; // All moters off initally
